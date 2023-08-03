ea59b21e4aa273764e0b94a022faafd39878e392
Taylan77 <taylanbarbosa73@gmail.com>
2023-08-03 19:51:07 -0300

    primeiro desafio

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 79
+console.log(`A velocidade do seu carro é de ${vel} Km/h`)
+if (vel > 60){
+    console.log(`Voce ultrapassou a velocidade permitida. MULTADO!`)
+} //Condiçao Simples!!!!!!!!!!
+console.log(`Dirija sempre usando o cinto de segurança!`)
