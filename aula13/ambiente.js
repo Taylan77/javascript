8136ac22fc481c258d8ac0ea30a6562b2e536b4c
Taylan77 <taylanbarbosa73@gmail.com>
2023-08-07 19:31:02 -0300

    att

---
+++ aula13/ambiente.js
@@ -0,0 +1,15 @@
+// c = 1
+// do {
+//     console.log (`Passo ${c}`)
+//     c++
+// } while (c < 10)
+
+
+
+
+
+ c = 1
+while (c <= 100){
+    console.log (` ${c} JESSI A MELHOR PESSOA DO MUNDO`)
+     c++
+}
