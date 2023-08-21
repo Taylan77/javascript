ca77cfc7881877997a848620c06db4f9698d25fc
Taylan77 <taylanbarbosa73@gmail.com>
2023-08-21 20:41:04 -0300

    correçao ex

--- aula14ex/exercicio03/script.js
+++ aula14ex/exercicio03/script.js
@@ -1,24 +1,19 @@
 function contar(){
-    var cont1 = document.getElementById('cont1')
-    var cont2 = document.getElementById('cont2')
+    var cont1 = document.getElementById('txti')
+    var cont2 = document.getElementById('txtf')
     var passo = document.getElementById('passo')
     var res = document.getElementById('res')
-    res.innerText += 'Contando..'
-    var cnt1 = Number(cont1.value)
-    var cnt2 = Number(cont2.value)
-    var pass = Number(passo.value)
-    if (cont1.value.length == 0) {
-        res.innerText = 'Impossivel contar'
-    }else if (pass == 0){
-        alert('Passo invalido, considerando passo = 1')
-        pass = 1
-        while (cnt1 <= cnt2){
-            res.innerText += `${cnt1}`
-            cnt1 = cnt1 + pass
-        }
-    }else{
-        for(;cnt1 <= cnt2; cnt1 = cnt1 + pass){
-            res.innerText += `${cnt1}`
-        }
+
+    if (cont1.value.length == 0 || cont2.value.length == 0 || passo.value.length == 0) {
+        alert('[ERRO] Faltam dados!!')
+    }else {
+        res.innerHTML = 'Contando: '
+        var i = Number(cont1.value)
+        var f = Number(cont2.value)
+        var p = Number(passo.value)
+
+        for (let c = i; c <= f; c += p ){
+            res.innerHTML += `${c}`
+        }   
     }
 }
