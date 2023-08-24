c245959bff24e339bd5e516a0bb8cba461005779
Taylan77 <taylanbarbosa73@gmail.com>
2023-08-24 19:44:55 -0300

    Update script.js

--- aula16ex/exercicio05/script.js
+++ aula16ex/exercicio05/script.js
@@ -1,12 +1,15 @@
 function adicionar(){
     var num = document.getElementById('txtn')
-    var valor = document.getElementById('selval')
     var nume = Number(num.value)
+    var array = []
 
     if( num.value.length <= 0 || nume > 100 || nume < 1){
         alert('Valor inválido ou já encontrado na lista.')
     }else{
         //TENHO QUE ARMAZENAR OS NUMEROS DENTRO DE UM ARRAY
+        var valor = document.getElementById('selval')
+        array.push(valor)
+        valor.innerHTML = `Valor ${valor} adicionado!`
     }
     
 }
