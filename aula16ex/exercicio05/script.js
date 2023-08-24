8676571233d32c86a2d262ea9b4f54ef0b7b737c
Taylan77 <taylanbarbosa73@gmail.com>
2023-08-24 20:53:42 -0300

    exercicio incompleto

--- aula16ex/exercicio05/script.js
+++ aula16ex/exercicio05/script.js
@@ -12,7 +12,16 @@ function adicionar(){
         let item = document.createElement('option')
         item.text = `Valor ${nume} adicionado.`
         valor.appendChild(item)
-
     }
     
 }
+
+function finalizar(){
+    var res = document.getElementById('res')
+    var maior = Math.max.apply(null, array)
+    res.innerHTML = `Ao todo, temos  números cadastrados.<br/><br/>`
+    res.innerHTML += `O maior valor informado foi .<br/><br/>`
+    res.innerHTML += `O menor valor informado foi .<br/><br/>`
+    res.innerHTML += `Somando todos os valores, temos .<br/><br/>`
+    res.innerHTML += `A média dos valores digitados é .<br/><br/>`
+} //TENHO QUE COLOCAR OS VALORES ACIMA
